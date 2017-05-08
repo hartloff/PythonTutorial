b39bfd434f0954cbafb6188ce55ffac37b8591e8
jessehartloff <hartloff@buffalo.edu>
2017-05-08 14:53:03 -0400

    initial commit

---
+++ PS4_DataStructures.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+def Q1(number_list):
+    pass
+    # TODO return number of ints in the list
+
+
+def Q2(string_list):
+    pass
+    # TODO add the string "movie" to the end of the list and return that list
+
+
+def Q3(number_list, index):
+    pass
+    # TODO return the element at index i from the input list
+    #      You can assume the input contains at least i+1 values
+
+
+def Q4(number_list, value):
+    pass
+    # TODO return true if value is in the input list, false otherwise
+
+
+def Q5(string_list):
+    pass
+    # TODO return a string containing every word of the input concatenated into a single String and separated by spaces
+    #      Example: If the input is ["war", "never", "changes"] the output is "war never changes"
+
+
+def Q6():
+    pass
+    # TODO Create and return an ArrayList of the integers 1 through 10 (including 1 and 10)
+
+
+def Q7():
+    pass
+    # TODO create and return an ArrayList of Strings containing the strings "memory", "is", "the", "key" in that order
+
+
+def Q8(string_int_dict, key, value):
+    pass
+    # TODO  add an entry into the map with the the given key and value
+
+
+def Q9(bool_int_dict, key):
+    pass
+    # TODO return the value in the input map at the given key
+
+
+def Q10(string_int_dict, value):
+    pass
+
+    # TODO return true if value is in the input map as a value at any key, false otherwise
+
+
+def Q11(string_double_dict):
+    pass
+    # TODO return the number of entries in the input map
+
+
+def Q12(string_int_dict):
+    pass
+    # TODO return a list of all values in the input map
+
+
+def Q13(string_int_dict):
+    pass
+    # TODO return a list of all keys in the input map
+
+
+def Q14(int_int_dict, key):
+    pass
+    # TODO return true if key is a key in the input map, false otherwise
+
+
+def Q15(int_int_dict, value):
+    pass
+    # TODO return the number of times value is in the input map as a value
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    pass
